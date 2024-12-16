6e62020edc111fecc6ce9274866775a2d99f1933
Barsha28 <116787199+Barsha28@users.noreply.github.com>
2024-12-16 19:25:06 +0530

    Update Map.java

--- src/com/test/Map.java
+++ src/com/test/Map.java
@@ -3,7 +3,7 @@ package com.test;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map.Entry;
-import java.util.Set;
+import java.util.*;
 
 public class Map {
 	public static void main(String[] args) {
