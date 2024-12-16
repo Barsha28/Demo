eca58d701558074e574e6b0d1b1a68e923bbd424
barsha28 <maharanabarsharani28@gmail.com>
2024-12-16 19:07:35 +0530

    Changes the LoginPage issue

--- src/com/test/Map.java
+++ src/com/test/Map.java
@@ -4,26 +4,22 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map.Entry;
 import java.util.Set;
-import java.util.*;
-import java.util.Set;
-import java.util.Set1;
-import java.util.**;
 
 public class Map {
-public static void main(String[] args) {
-	HashMap<Integer, String>m1=new HashMap<Integer, String>();
-	m1.put(100, "dsmnc");
-	m1.put(102, "scnsn");
-	m1.put(105, "cdkc");
-	m1.put(105, "fedf");
-	System.out.println(m1);
-	System.out.println(m1.get(102));
-	Set<Entry<Integer, String>> m2 = m1.entrySet();
-	System.out.println(m2);
-	Iterator<Entry<Integer, String>> it= m2.iterator();
-	while(it.hasNext()) {
-		System.out.println(it.next());
+	public static void main(String[] args) {
+		HashMap<Integer, String> m1 = new HashMap<Integer, String>();
+		m1.put(100, "dsmnc");
+		m1.put(102, "scnsn");
+		m1.put(105, "cdkc");
+		m1.put(105, "fedf");
+		System.out.println(m1);
+		System.out.println(m1.get(102));
+		Set<Entry<Integer, String>> m2 = m1.entrySet();
+		System.out.println(m2);
+		Iterator<Entry<Integer, String>> it = m2.iterator();
+		while (it.hasNext()) {
+			System.out.println(it.next());
+		}
+
 	}
-	
-}
 }
